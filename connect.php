ae3bf289abfa04c7ea202a795275ba7922f101fa
admin <admin@OWI-68.local>
2016-05-18 11:48:22 +0200

    Try/catch debug

--- connect.php
+++ connect.php
@@ -7,7 +7,11 @@
         $pass = 'mysql';
         $pdo = new PDO('mysql:host=localhost;dbname=scavanger_hunt', $user, $pass);
     } else {
-        $pdo = new PDO($dburl);
+        try {
+            $pdo = new PDO($dburl);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
     }
 
 ?>
